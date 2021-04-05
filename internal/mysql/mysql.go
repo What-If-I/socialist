6fc1b79007294b7f9579c9a3e5903c728f373476
Denis Fetinin <denka0087@gmail.com>
2021-04-05 21:22:56 +0300

    Limit connections pool

--- internal/mysql/mysql.go
+++ internal/mysql/mysql.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"fmt"
+	"time"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jmoiron/sqlx"
@@ -12,6 +13,9 @@ func NewDB(conn string) (*sqlx.DB, error) {
 	if err != nil {
 		return nil, fmt.Errorf("failed to connect to %s: %w", conn, err)
 	}
+	db.SetConnMaxLifetime(30 * time.Second)
+	db.SetMaxOpenConns(150)
+	db.SetMaxIdleConns(15)
 
 	if err := db.Ping(); err != nil {
 		return nil, err
