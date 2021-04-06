88bfbbf40eefdbc70180c26e7b25c83705214490
Denis Fetinin <denka0087@gmail.com>
2021-04-06 10:31:13 +0300

    Add index for user name and surname

--- migrations/002_index_user_search.down.sql
+++ migrations/002_index_user_search.down.sql
@@ -1,2 +1,1 @@
-DROP INDEX profile_name_btree ON profile;
-DROP INDEX profile_surname_btree ON profile;
+DROP INDEX profile_name_surname_btree ON profile;
--- migrations/002_index_user_search.sql
+++ migrations/002_index_user_search.sql
@@ -1,2 +1,1 @@
-CREATE INDEX profile_name_btree USING BTREE ON profile (name);
-CREATE INDEX profile_surname_btree USING BTREE ON profile (surname);
+CREATE INDEX profile_name_surname_btree USING BTREE ON profile (name, surname);
